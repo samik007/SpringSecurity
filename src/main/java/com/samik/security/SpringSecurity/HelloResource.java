f36f43c436bbbbb4d36ab478a23a134b86e9f27f
39623 <39623@EBI139TFEW.corp.hexaware.com>
2018-07-11 12:36:50 +0530

    Changes added

--- src/main/java/com/samik/security/SpringSecurity/HelloResource.java
+++ src/main/java/com/samik/security/SpringSecurity/HelloResource.java
@@ -1,15 +1,16 @@
-package resource;
+package com.samik.security.SpringSecurity;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/rest/hello")
 public class HelloResource {
-	
-	@GetMapping
-	public String hello() {
-		return "Hello World";
-	}
+
+    @GetMapping
+    public String hello() {
+    	return "Hello Check";
+    }
 }
